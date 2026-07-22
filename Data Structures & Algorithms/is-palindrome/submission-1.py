89f803638bcfb954818f4047b3a3d400987b8a75
priyanshuu-dev <p7061815411@gmail.com>
2026-07-22 09:56:06 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned=""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                cleaned+=s[i]
+                cleaned=cleaned.lower()
+        low=0
+        high=len(cleaned)-1
+
+        while low<=high:
+            if cleaned[low]==cleaned[high]:
+                low+=1
+                high-=1
+
+            else:
+                return False
+
+        return True
+
+            
+
+       
+
+           
+
+        
+
+        
+
+
+
+
+
+
+        
+                
+
+
+
+        
+        
+        
+
+        
